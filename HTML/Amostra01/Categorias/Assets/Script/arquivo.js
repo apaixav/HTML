9b5f704b7b7b4966107f1d18ada3b72bae4a1da6
apaixav <lucasrib01@gmail.com>
2023-10-19 15:13:14 -0300

    JS

---
+++ HTML/Amostra01/Categorias/Assets/Script/arquivo.js
@@ -0,0 +1,29 @@
+let valor = 3
+
+let valor2 = "3"
+
+let nome = "João"
+
+const url = "http://google.com.br"
+console.log(valor)
+console.log(typeof valor)
+
+console.log(typeof valor2)
+
+console.log(url)
+
+function ola(){
+    console.log("olá mundo")
+}
+ola()
+function soma(numero1,numeor2){
+    return numero1 + numeor2
+}
+console.log(soma(3,5))
+
+const olaMundo = () => console.log("olá Mundo 2")
+
+olaMundo()
+
+const somaNum = (num1,num2) => num1+num2
+console.log(somaNum(45,54))
---
+++ HTML/Amostra01/Categorias/Assets/Script/script.js
@@ -0,0 +1,59 @@
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+
+
+
+function validarNome () {
+    let txt = document.querySelector('#txtNome')
+ if (nome.value.length <3) {
+    txt.innerHTML = 'nome inválido'
+    txt.style.color = 'red'
+ }else {
+    txt.innerHTML = 'nome válido'
+    txt.style.color = 'green'
+    nomeOk = true
+
+ }
+
+}
+
+function validarEmail () {
+    let txtEmail = document.querySelector('#txtEmail')
+ if (email.value.indexOf('@') == -1 || email.value.indexOf('.') ==-1) {
+    txtEmail.innerHTML = 'E-mail inválido'
+    txtEmail.style.color = 'red'
+ }else {
+    txtEmail.innerHTML = 'E-mail válido'
+    txtEmail.style.color = 'green'
+    emailOk = true
+
+ }
+
+}
+
+function validarAssunto () {
+    let txtAssunto = document.querySelector('#txtAssunto')
+ 
+    if (assunto.value.length >= 100) {
+    txtAssunto.innerHTML = 'Texto é muito grande, digite no máximo 100 caracteres'
+    txtAssunto.style.color = 'red'
+    txtAssunto.style.display = 'block'
+    }else {
+    txtAssunto.style.display = 'none'
+    assuntoOk = true
+
+ }
+
+}
+
+function enviar(){
+    if (nomeOk == true && emailOk == true && assuntoOk == true){
+        alert('Formulário enviado com sucesso!')
+    } else {
+        alert('Preencha o formulário correntamente antes de enviar...')
+    }
+} 
